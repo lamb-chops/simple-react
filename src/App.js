beb324aff30e97997d3156aeffd120d411a177a3
David Lambert <david.lambert@my.utsa.edu>
2022-09-16 15:34:25 -0500

    #34

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p> this is also visable </p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>title</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
