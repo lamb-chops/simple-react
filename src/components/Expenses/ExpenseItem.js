8babeed605c22e16d186cc1e56b947ea7aa8eae2
David Lambert <david.lambert@my.utsa.edu>
2022-09-20 17:51:51 -0500

    added dropdown filter component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,20 +5,13 @@ import Card from "../UI/Card";
 import { useState } from "react";
 
 function ExpenseItem(props) {
-  //react hook, set init value, returns 2 value array that contains value itself and func to update
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle('Updated')
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
